5156fcc634435949356ab780e5a8c0a49f216c5c
sonvq <sonvq@gemvietnam.com>
2017-09-05 19:33:47 +0700

    commit todolist code

---
+++ js/app.js
@@ -0,0 +1,25 @@
+
+angular.module('todolist', ['ngRoute', 'ngResource'])
+	.config(function ($routeProvider) {
+		'use strict';
+
+		var routeConfig = {
+			controller: 'TodoCtrl',
+			templateUrl: 'todo-index.html',
+			resolve: {
+				store: function (todoStorage) {
+					return todoStorage.then(function (module) {
+						module.get();
+						return module;
+					});
+				}
+			}
+		};
+
+		$routeProvider
+			.when('/', routeConfig)
+			.when('/:status', routeConfig)
+			.otherwise({
+				redirectTo: '/'
+			});
+	});
